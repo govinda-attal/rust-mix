797a5f31362b55451dda5bd1cc922c20ec795f48
Govinda Attal <govinda.attal@oracle.com>
2022-12-29 02:27:47 +1100

    rust: lc - 409 - longest palindrome

--- algos/leetcode/src/lc_409_longest_palindrome.rs
+++ algos/leetcode/src/lc_409_longest_palindrome.rs
@@ -13,7 +13,7 @@ impl Solution {
         let mut has_odd = false;
         for val in hm.values() {
             if *val % 2 == 0 {
-                max_len += val
+                max_len += val;
             } else {
                 max_len += val - 1;
                 has_odd = true;
