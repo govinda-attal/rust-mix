bee9aa972678e43314e31036c7aebfa92da09705
Govinda Attal <govinda.attal@oracle.com>
2022-12-28 16:15:31 +1100

    rust: lc - 020 - valid paranthesis

---
+++ algos/leetcode/src/lc_020_valid_paranthesis.rs
@@ -0,0 +1,45 @@
+pub fn is_valid(input: &str) -> bool {
+    let mut stack = String::new();
+
+    for ch in input.chars() {
+        match ch {
+            '{' => stack.push('}'),
+            '(' => stack.push(')'),
+            '[' => stack.push(']'),
+            '}'|')'|']' => {
+                let popped = stack.pop().unwrap_or_default();
+               
+                if popped != ch {
+                    return false;
+                }
+            },
+            _ => {
+                return false
+            }
+        }
+    }
+    true
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn check_valid() {
+        let input = r"()[]{}";
+        assert!(is_valid(input))
+    }
+
+    #[test]
+    fn check_empty() {
+        let input = r"";
+        assert!(is_valid(input))
+    }
+
+    #[test]
+    fn check_invalid() {
+        let input = r"{{))";
+        assert!(!is_valid(input))
+    }
+}
--- algos/leetcode/src/lib.rs
+++ algos/leetcode/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod utils;
 
+pub mod lc_020_valid_paranthesis;
 pub mod lc_226_invert_tree_node;
